ebcdaade92fddce7dfc57dfde6a3e778d703c3d3
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-09-21 21:00:14 +0700

    cleanup add styling

--- src/components/Particles/Particles.tsx
+++ src/components/Particles/Particles.tsx
@@ -1,38 +1,47 @@
 /* eslint-disable no-restricted-globals */
 import { useEffect } from 'react';
 import * as d3 from 'd3';
+import { makeStyles } from 'src/makeStyles';
 
-import './particles.css';
+const useStyles = makeStyles()(() => ({
+  root: {
+    '& rect': {
+      fill: 'none',
+      pointerEvents: 'all',
+    },
+    '& circle': {
+      fill: 'none',
+      strokeWidth: '2.5px',
+    },
+  },
+}));
 
 const Particles: React.FC = (props) => {
+  const { classes } = useStyles();
+
   useEffect(() => {
     const width = Math.max(960, innerWidth);
     const height = Math.max(500, innerHeight);
 
     let i = 0;
 
-    const svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
+    const svg = d3.select('body').append('svg').attr('width', width).attr('height', height).attr('class', classes.root);
 
     svg
       .append('rect')
-      // .attr('class', classes.rect)
       .attr('width', width)
       .attr('height', height)
-
       .on('ontouchstart' in document ? 'touchmove' : 'mousemove', particle);
 
     function particle(event) {
       const m = d3.pointer(event);
-      console.log('particle');
 
       svg
         .insert('circle', 'rect')
-        // .attr('class', classes.circle)
         .attr('cx', m[0])
         .attr('cy', m[1])
         .attr('r', 1e-6)
         .style('stroke', d3.hsl((i = (i + 1) % 360), 1, 0.5) as any)
-
         .style('stroke-opacity', 1)
         .transition()
         .duration(2000)
