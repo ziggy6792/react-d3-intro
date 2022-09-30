8f4ad19af21d814c055b77ccc77a178123faa5fc
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-09-30 16:02:47 +0800

    finished demo

--- src/components/Particles/Particles.tsx
+++ src/components/Particles/Particles.tsx
@@ -1,32 +1,56 @@
 /* eslint-disable no-restricted-globals */
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
+import { TSelection } from 'src/d3Types';
+import { makeStyles } from 'src/makeStyles';
+
+const useStyles = makeStyles()(() => ({
+  root: {
+    background: '#222',
+  },
+  rect: {
+    fill: 'none',
+    pointerEvents: 'all',
+  },
+  circleContainer: {
+    '& circle': {
+      fill: 'none',
+      strokeWidth: '2.5px',
+    },
+  },
+}));
 
 // eslint-disable-next-line arrow-body-style
 export const Particles: React.FC = (props) => {
+  const [svg, setSvg] = useState<null | TSelection>(null);
+  const svgRef = useRef<null | SVGSVGElement>(null);
+
+  const width = Math.max(960, innerWidth);
+  const height = Math.max(500, innerHeight);
+
+  const { classes } = useStyles();
+
   useEffect(() => {
-    const width = Math.max(960, innerWidth);
-    const height = Math.max(500, innerHeight);
+    if (!svg) {
+      setSvg(d3.select(svgRef.current));
+      return;
+    }
 
     let i = 0;
 
-    const svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
+    svg.select(`.${classes.rect}`).on('ontouchstart' in document ? 'touchmove' : 'mousemove', particle);
 
-    svg
-      .append('rect')
-      .attr('width', width)
-      .attr('height', height)
-      .on('ontouchstart' in document ? 'touchmove' : 'mousemove', particle);
+    const circleContainer = svg.select(`.${classes.circleContainer}`);
 
     function particle(event) {
-      const m = d3.pointer(this);
+      const m = d3.pointer(event);
 
-      svg
-        .insert('circle', 'rect')
+      circleContainer
+        .append('circle')
         .attr('cx', m[0])
         .attr('cy', m[1])
         .attr('r', 1e-6)
-        .style('stroke', d3.hsl((i = (i + 1) % 360), 1, 0.5))
+        .style('stroke', d3.hsl((i = (i + 1) % 360), 1, 0.5).toString())
         .style('stroke-opacity', 1)
         .transition()
         .duration(2000)
@@ -35,9 +59,15 @@ export const Particles: React.FC = (props) => {
         .style('stroke-opacity', 1e-6)
         .remove();
 
-      d3.event.preventDefault();
+      event.preventDefault();
     }
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [svg]);
 
-  return <div>hi</div>;
+  return (
+    <svg ref={svgRef} width={width} height={height} className={classes.root}>
+      <g className={classes.circleContainer} />
+      <rect width={width} height={height} className={classes.rect} />
+    </svg>
+  );
 };
